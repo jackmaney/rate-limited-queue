6bed69886014ce9b404f192177e00c971a9199d0
Jack Maney <jackmaney@gmail.com>
2015-01-08 20:31:01 -0600

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(name='rate_limited_queue',
+      version='0.0.1',
+      description='A queue that allows for processing of items within one or more rate-limits.',
+      url='https://github.com/jackmaney/rate-limited-queue',
+      author='Jack Maney',
+      author_email='jackmaney@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      zip_safe=False)
